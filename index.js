68dac258c0de8e904b68e1dd5ea6302b3cdecba2
Dfisher <60874439+Drax-Fisher@users.noreply.github.com>
2020-12-16 11:09:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,27 @@ document.onreadystatechange = function() {
         document.querySelector("body").style.visibility = "visible"; 
     } 
 };
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+	var navh = document.getElementsByTagName('nav')[0].offsetHeight;
+  	if (document.body.scrollTop > Number(navh)+20 || document.documentElement.scrollTop > Number(navh)+20) {
+ 		var ssb = document.getElementById('search_bar');
+ 		ssb.style.width = "100%";
+ 		ssb.style.position = "fixed";
+ 		ssb.style.top = "0px";   
+ 		ssb.style.left = "0px";
+ 		ssb.style.right = "0px";
+ 		ssb.style.margin = "auto";	
+  	} else {
+  		var ssb = document.getElementById('search_bar');
+ 		ssb.style.width = "200px";
+ 		ssb.style.position = "relative";
+ 		// ssb.style.top = "initial";
+ 		// ssb.style.left = "initial";
+ 		// ssb.style.right = "initial";
+  	}
+}
 function menu(el){
 	if (el.className=="close") {
 		el.style.bottom = "30%";
@@ -41,14 +62,15 @@ function check(e, el) {
 	var key = e.keyCode ? e.keyCode : e.which;
 	if (key==13) {
 		var inp = document.getElementById('search_bar');
-		var cat = inp.value;
+		var cat = inp.value.toLowerCase();
 		inp.value = "";
 		if (cat.length>0) {
 			var flag = 0;
+			document.querySelector('#content div:first-child').style.display = "inline-block";
 			var allimg = document.querySelectorAll('#content div img');
 			for (var i = 0; i < allimg.length; i++) {
 				if (flag == 1) {
-					document.querySelector("#content p").innerHTML = "Search results for '"+cat+"':";
+					document.querySelector("#content p").innerHTML = "'"+cat+"'";
 				}
 				var cname = allimg[i].className;
 				if (cname.includes(cat)) {
@@ -65,4 +87,26 @@ function check(e, el) {
 		}
 	}
 
+}
+function search(el){
+	cat = el.innerHTML.toLowerCase();
+	var flag = 0;
+	document.querySelector('#content div:first-child').style.display = "inline-block";
+	var allimg = document.querySelectorAll('#content div img');
+	for (var i = 0; i < allimg.length; i++) {
+		if (flag == 1) {
+			document.querySelector("#content p").innerHTML = "'"+cat+"'";
+		}
+		var cname = allimg[i].className;
+		if (cname.includes(cat)) {
+			allimg[i].style.display = "inline-block";
+			flag = 1;
+		}
+		else{
+			allimg[i].style.display = "none";
+		}
+	}
+	if (flag==0) {
+		document.querySelector("#content p").innerHTML = "There are no search results for '"+cat+"'";
+	}
 }
